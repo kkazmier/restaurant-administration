f902749e1744ff22a2ee29f479eb171c85ede9ee
kkazm <s14005@pjwstk.edu.pl>
2022-06-09 10:50:08 +0200

    Add mapping path.

--- src/main/java/gastro/com/restaurantadministration/dish/DishRestController.java
+++ src/main/java/gastro/com/restaurantadministration/dish/DishRestController.java
@@ -1,14 +1,12 @@
 package gastro.com.restaurantadministration.dish;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("${api.v1.rest}" + "/dish")
 public class DishRestController {
     private final DishServiceImpl dishService;
 
