512532e5dd45a7e35caf99411e29e1f90817c6d0
kkazm <s14005@pjwstk.edu.pl>
2022-06-12 14:59:49 +0200

    Add methods.

--- src/main/java/gastro/com/restaurantadministration/dish/DishController.java
+++ src/main/java/gastro/com/restaurantadministration/dish/DishController.java
@@ -1,17 +1,23 @@
 package gastro.com.restaurantadministration.dish;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
-@RequestMapping("${api.v1.rest}" + "/dish")
+@RequestMapping("${api.v1}" + "/dish")
 public class DishController {
+    private static final Logger logger = LoggerFactory.getLogger(DishController.class);
     private final DishServiceImpl dishService;
 
     @Autowired
@@ -24,8 +30,19 @@ public class DishController {
         return dishService.getListAllDishes();
     }
 
+    @GetMapping("/addDish")
+    public String showForm(Model model, Dish dish) {
+        model.addAttribute("dishForm", dish);
+        logger.info("[get]get dish");
+        return "forms/dish-form";
+    }
+
     @PostMapping("/addDish")
-    Dish addDish(@RequestBody Dish dish){
-        return dishService.addDish(dish);
+    public String checkDish(@Valid Dish dish, BindingResult bindingResult) {
+        logger.info("[post]get dish");
+        if (bindingResult.hasErrors()) {
+            return "dish-form";
+        }
+        return "redirect:/results";
     }
 }
