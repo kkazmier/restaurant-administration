bae9f825d0df1fbc1a1be8b40373c368a960444a
Karol <poczta.karol.kazmierczak@gmail.com>
2022-06-26 15:48:43 +0200

    Add/edit methods.

--- src/main/java/gastro/com/restaurantadministration/bill/BillController.java
+++ src/main/java/gastro/com/restaurantadministration/bill/BillController.java
@@ -1,5 +1,6 @@
 package gastro.com.restaurantadministration.bill;
 
+import gastro.com.restaurantadministration.dish.Dish;
 import gastro.com.restaurantadministration.dish.DishServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -24,13 +26,30 @@ public class BillController {
 
     @GetMapping("/newBill")
     public String newBill(Model model) {
+        Bill newBill = new Bill();
         model.addAttribute("allDishes", dishService.getListAllDishes());
+        model.addAttribute("newBill", newBill);
         return "prepare-bill";
     }
 
     @GetMapping("/addDish/{dishId}/ToBill/{billId}")
-    public String addDishToBill(Model model) {
+    public String addDishToBill(@PathVariable Long dishId, @PathVariable Long billId,Model model) {
+        Bill bill = (Bill) model.getAttribute("newBill");
+        Dish dish = (Dish) model.getAttribute("addedDish");
+        if (bill != null && dish != null) {
+            billService.addDishToBill(dish, bill);
+            logger.info(dish.name + " added.");
+        } else {
+            logger.warn("Bill and/or dish not exist.");
+        }
         model.addAttribute("allDishes", dishService.getListAllDishes());
+        model.addAttribute("newBill", bill);
         return "prepare-bill";
+    }
+
+    @GetMapping("/all")
+    public String getAllBills(Model model) {
+        model.addAttribute("allBills", billService.getAllBills());
+        return "all-bills";
     }
 }
