ad2bad33159ea6a92cb6c8ac5c7044cdcf0ffb3b
Karol <poczta.karol.kazmierczak@gmail.com>
2022-07-06 13:57:38 +0200

    First commit.

---
+++ src/main/java/gastro/com/restaurantadministration/delivery/DeliveryMan.java
@@ -0,0 +1,19 @@
+package gastro.com.restaurantadministration.delivery;
+
+import gastro.com.restaurantadministration.model.RestaurantWorker;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "dishes", schema = "public")
+public class DeliveryMan extends RestaurantWorker {
+}
---
+++ src/main/java/gastro/com/restaurantadministration/manager/Manager.java
@@ -0,0 +1,20 @@
+package gastro.com.restaurantadministration.manager;
+
+import gastro.com.restaurantadministration.model.RestaurantWorker;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "dishes", schema = "public")
+public class Manager extends RestaurantWorker {
+
+}
