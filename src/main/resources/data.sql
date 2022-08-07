6f791bfc388177e8de1396eb07768e8290cbe164
kkazmier <dev.karol.kazmierczak@gmail.com>
2022-08-07 09:16:43 +0200

    Change database to SQLite +configuration.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,1 @@
-create table dishes(id int, name varchar(32), primary key(id));
-INSERT INTO dishes (id, name) VALUES ('1', 'dish');
+INSERT INTO dishes (id, name, description, price) VALUES ('1', 'dish', 'dec...', '20.00');
