0b5d0436e59871655d3a97bbc6df704d9f702374
kkazmier <dev.karol.kazmierczak@gmail.com>
2022-08-11 19:32:08 +0200

    Fix null form error.

--- src/main/java/gastro/com/restaurantadministration/dish/DishController.java
+++ src/main/java/gastro/com/restaurantadministration/dish/DishController.java
@@ -9,12 +9,13 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.validation.Valid;
 
 @Controller
 @RequestMapping("${api.v1}" + "/dish")
-public class DishController {
+public class DishController implements WebMvcConfigurer {
     private static final Logger logger = LoggerFactory.getLogger(DishController.class);
     private final DishServiceImpl dishService;
 
@@ -38,10 +39,11 @@ public class DishController {
     }
 
     @PostMapping("/addDish")
-    public String checkDish(@Valid Dish dish, BindingResult bindingResult) {
+    public String checkDish(@Valid Dish dish, BindingResult bindingResult, Model model) {
         logger.info("[post]get dish");
         if (bindingResult.hasErrors()) {
-            return "dish-form";
+            model.addAttribute("dishForm", dish);
+            return "forms/dish-form";
         }
         dishService.addDish(dish);
         return "redirect:/api/v1/dish/allDishes";
