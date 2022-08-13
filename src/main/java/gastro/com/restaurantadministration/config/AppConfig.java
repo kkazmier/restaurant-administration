59282449b9e59a8b24e4cf172e9b2e3a151fc3ae
kkazmier <dev.karol.kazmierczak@gmail.com>
2022-08-13 12:56:50 +0200

    First commit.

---
+++ src/main/java/gastro/com/restaurantadministration/config/AppConfig.java
@@ -0,0 +1,14 @@
+package gastro.com.restaurantadministration.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("api/v1/").setViewName("index");
+        registry.addViewController("api/v1/index").setViewName("index");
+    }
+}
