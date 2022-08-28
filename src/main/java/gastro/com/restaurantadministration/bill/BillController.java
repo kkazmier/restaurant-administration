168ea27b6d30531844f5fdbb5150b8ddf3bc7ef4
kkazmier <dev.karol.kazmierczak@gmail.com>
2022-08-28 11:37:43 +0200

    Impl add dish to bill.

--- src/main/java/gastro/com/restaurantadministration/bill/BillController.java
+++ src/main/java/gastro/com/restaurantadministration/bill/BillController.java
@@ -6,21 +6,24 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("${api.v1}" + "/bill")
+@SessionAttributes("newBill")
 public class BillController {
     private static final Logger logger = LoggerFactory.getLogger(BillController.class);
 
     private final DishServiceImpl dishService;
 
-
     @Autowired
     public BillController(DishServiceImpl dishService) {
         this.dishService = dishService;
+    }
+
+    @ModelAttribute("newBill")
+    public Bill newBill() {
+        return new Bill();
     }
 
     @GetMapping("/bills")
@@ -29,15 +32,24 @@ public class BillController {
     }
 
     @GetMapping("/newBill")
-    public String newBill(Model model) {
+    public String newBill(Model model, @ModelAttribute("newBill") Bill newBill) {
+        if(newBill != null) {
+            newBill = new Bill();
+        }
         model.addAttribute("allDishes", dishService.getListAllDishes());
         return "new-bill";
     }
 
-    @GetMapping("/addDish/{dishId}/ToBill/{billId}")
-    public String addDishToBill(@PathVariable("dishId") Long dishId, @PathVariable("billId") Long billId, Model model) {
-        model.addAttribute("allDishes", dishService.getListAllDishes());
+    @GetMapping("/addDish/{dishId}/toBill")
+    public String addDishToBill(@ModelAttribute("newBill") Bill newBill,
+                                @PathVariable("dishId") Long dishId, Model model) {
+        if(newBill != null) {
+            newBill = new Bill();
+        }
 
+        model.addAttribute("allDishes", dishService.getListAllDishes());
+        logger.info("Add dish " + dishId + " to bill ");
+        logger.info(String.valueOf(newBill.getDishes().size()));
         return "new-bill";
     }
 }
